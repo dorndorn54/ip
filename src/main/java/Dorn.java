4a6081d80220dcf36c74ef75ba90d1cb06e4cb8c
dorndorn54 <samuellee20523@gmail.com>
2026-01-22 22:40:55 +0800

    removed traces of Duke and added my own hello interface

---
+++ src/main/java/Dorn.java
@@ -0,0 +1,17 @@
+public class Dorn {
+    public static void main(String[] args) {
+        String logo = " ____   ___  ____  _   _ \n"
+                + "|  _ \\ / _ \\|  _ \\| \\ | |\n"
+                + "| | | | | | | |_) |  \\| |\n"
+                + "| |_| | |_| |  _ <| |\\  |\n"
+                + "|____/ \\___/|_| \\_\\_| \\_|\n";
+        String name = "Dorn";
+
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("");
+        System.out.println(logo);
+        System.out.println("");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
