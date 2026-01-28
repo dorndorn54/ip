ade03946af9c837235179b41aa3a5587a1b99eb0
dorndorn54 <samuellee20523@gmail.com>
2026-01-28 22:37:09 +0800

    Level 1. Echo

--- src/main/java/Dorn.java
+++ src/main/java/Dorn.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Dorn {
-    public static void main(String[] args) {
+    public static void initalisation(){
         String logo = " ____   ___  ____  _   _ \n"
                 + "|  _ \\ / _ \\|  _ \\| \\ | |\n"
                 + "| | | | | | | |_) |  \\| |\n"
@@ -7,11 +9,32 @@ public class Dorn {
                 + "|____/ \\___/|_| \\_\\_| \\_|\n";
         String name = "Dorn";
 
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println("");
-        System.out.println(logo);
-        System.out.println("");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm " + name +
+                "\nWhat can I do for you?" +
+                "\n\n" + logo + "\n");
+    }
+
+    public static void receiveInputAndPrint(){
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
+            line = in.nextLine();
+
+            if(line.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println("\n" + line + "\n");
+        }
+
+        in.close(); // close scanner
+    }
+
+    public static void main(String[] args) {
+        initalisation();
+
+        receiveInputAndPrint();
     }
 }
